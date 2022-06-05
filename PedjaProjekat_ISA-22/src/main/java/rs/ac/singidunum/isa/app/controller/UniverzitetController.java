729d84ab5563bba88940492ef300d33ba35edaab
Pedja1201 <radakpredrag10@gmail.com>
2022-06-05 23:57:03 +0200

    Pokusaj OneToOne kod zvanja

--- PedjaProjekat_ISA-22/src/main/java/rs/ac/singidunum/isa/app/controller/UniverzitetController.java
+++ PedjaProjekat_ISA-22/src/main/java/rs/ac/singidunum/isa/app/controller/UniverzitetController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import rs.ac.singidunum.isa.app.aspect.Logged;
 import rs.ac.singidunum.isa.app.aspect.LoggedUniverzitet;
 import rs.ac.singidunum.isa.app.dto.AdresaDTO;
 import rs.ac.singidunum.isa.app.dto.NastavnikDTO;
