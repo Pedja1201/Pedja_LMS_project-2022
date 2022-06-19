1a31b4e1c67783a1b5fca756c752d2eb3ae37826
Pedja_r <predrag.radak.19@singimail.rs>
2022-06-19 21:17:32 +0200

    Stilizovanje tabele - ID prikaz

--- Project_KWA22/project/src/app/page/adrese/tabela-adrese/tabela-adrese.component.ts
+++ Project_KWA22/project/src/app/page/adrese/tabela-adrese/tabela-adrese.component.ts
@@ -1,3 +1,4 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
@@ -10,9 +11,24 @@ import { AdreseService } from 'src/app/service/adrese.service';
   styleUrls: ['./tabela-adrese.component.css']
 })
 export class TabelaAdreseComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'ulica', 'broj', 'mesto', "akcije"];
+  displayedColumns: string[] = ['select', 'id', 'ulica', 'broj', 'mesto', "akcije"];
   // dataSource : AdresaPage<Adresa> | undefined; //U dataSource je problem prilikom dobavljanja za backend
   dataSource = new MatTableDataSource();
+  selection = new SelectionModel<AdresaPage<Adresa>>(true, []);
+
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.elementi.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.elementi.forEach(row => this.selection.select(row));
+  }
 
   title="Tabela Adresa";
 
