943b0a16e964b88813cd1810ef9d11e138baaf9b
Pedja1201 <you@example.com>
2022-06-19 13:49:33 +0200

    Podignut PredmetTDb za skladistenje

--- WBIS_TDB_Project-22/src/main/java/rs/ac/singidunum/wbis/app/PredmetTDB.java
+++ WBIS_TDB_Project-22/src/main/java/rs/ac/singidunum/wbis/app/PredmetTDB.java
@@ -10,32 +10,33 @@ import org.apache.jena.update.UpdateExecutionFactory;
 import org.apache.jena.update.UpdateProcessor;
 import org.apache.jena.update.UpdateRequest;
 
-public class NastavnikTDB {
+public class PredmetTDB {
     public static void main(String[] args) {
+
         //Kreiramo triplet koji ubacujemo u RDF store
-        Node subject = NodeFactory.createURI("http://www.singidunum.ac.rs/fakultet#pedja");
+        Node subject = NodeFactory.createURI("http://www.singidunum.ac.rs/predmet#predmet1");
         Node predicate = NodeFactory.createURI("http://www.w3.org/1999/02/22-rdf-syntax-ns#type");
-        Node object = NodeFactory.createURI("http://www.singidunum.ac.rs/fakultet#Profesor");
+        Node object = NodeFactory.createURI("http://www.singidunum.ac.rs/predmet#Predmet");
         //Da bismo menjali sadrzaj store-a koristimo UpdateBuilder
         UpdateBuilder ub = new UpdateBuilder()
                 //insert komanda za ubacivanje
                 .addInsert(subject, predicate, object);
 
         try {
-            //kreiramo request, i saljemo na remote lokaciju
+            //kreiramo request, i �aljemo na remote lokaciju
             UpdateRequest req = ub.buildRequest();
-            UpdateProcessor u = UpdateExecutionFactory.createRemote(req, "http://localhost:3030/nastavnici");
+            UpdateProcessor u = UpdateExecutionFactory.createRemote(req, "http://localhost:3030/predmeti");
             u.execute();
         }
         catch(Exception e){
             e.printStackTrace();
         }
-        //primer dobavljanja koristeci string upit
-        String query = "SELECT ?subjekat WHERE { ?subjekat  <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>  <http://www.singidunum.ac.rs/fakultet#Profesor> }";
+        //primer dobavljanja koriste�i string upit
+        String query = "SELECT ?subjekat WHERE { ?subjekat  <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>  <http://www.singidunum.ac.rs/predmet#Predmet> }";
 
         //koristimo sparqlService za slanje zahteva preko http
         QueryExecution q = QueryExecutionFactory
-                .sparqlService("http://localhost:3030/nastavnici", query);
+                .sparqlService("http://localhost:3030/predmeti", query);
         //kao odgovor dobijamo ResultSet kroz koji iteriramo
         ResultSet results = q.execSelect();
         while (results.hasNext()) {
@@ -50,16 +51,18 @@ public class NastavnikTDB {
         SelectBuilder sb = new SelectBuilder()
                 .setDistinct(true)
                 .addPrefix("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
-                .addPrefix("prim", "http://www.singidunum.ac.rs/fakultet#")
+                .addPrefix("predmet", "http://www.singidunum.ac.rs/predmet#")
                 .addVar("?subjekat")
-                .addWhere("?subjekat", "rdf:type", "prim:Profesor");
+                .addWhere("?subjekat", "rdf:type", "predmet:Predmet");
         Query q2 = sb.build();
-        QueryExecution qe2 = QueryExecutionFactory.sparqlService("http://localhost:3030/nastavnici", q2);
+        QueryExecution qe2 = QueryExecutionFactory.sparqlService("http://localhost:3030/predmeti", q2);
         ResultSet rs = qe2.execSelect();
         while (rs.hasNext()) {
             QuerySolution solution = rs.nextSolution();
             RDFNode subjekat = solution.get("subjekat");
             System.out.println(subjekat);
         }
+
+
     }
 }
