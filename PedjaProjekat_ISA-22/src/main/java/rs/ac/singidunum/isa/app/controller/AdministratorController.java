4009f6651a0600818a94764f3587fff4fd54013a
Pedja_r <predrag.radak.19@singimail.rs>
2022-06-21 13:30:11 +0200

    Power Point prezentacija Servera

--- PedjaProjekat_ISA-22/src/main/java/rs/ac/singidunum/isa/app/controller/AdministratorController.java
+++ PedjaProjekat_ISA-22/src/main/java/rs/ac/singidunum/isa/app/controller/AdministratorController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import rs.ac.singidunum.isa.app.aspect.LoggedAdministrator;
@@ -25,7 +26,7 @@ public class AdministratorController {
 
     @LoggedAdministrator
     @RequestMapping(path = "", method = RequestMethod.GET)
-//        @Secured({"ROLE_ADMIN"})
+//    @Secured({"ROLE_ADMIN"})
     public ResponseEntity<Page<AdministratorDTO>> getAllAdministrator(Pageable pageable) {
         Page<Administrator> administrator = administratorService.findAll(pageable);
         Page<AdministratorDTO> administratori = administrator.map(new Function<Administrator, AdministratorDTO>() {
@@ -41,7 +42,7 @@ public class AdministratorController {
     }
 
     @RequestMapping(path = "/{administratorId}", method = RequestMethod.GET)
-//        @Secured({"ROLE_ADMIN"})
+//    @Secured({"ROLE_ADMIN"})
     public ResponseEntity<AdministratorDTO> getAdministrator(@PathVariable("administratorId") Long administratorId) {
         Optional<Administrator> administrator = administratorService.findOne(administratorId);
         if (administrator.isPresent()) {
